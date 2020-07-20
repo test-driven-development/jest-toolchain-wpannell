03922290c5d400bcc7c7ac08f50ccfce9945616e
wpannell <wil.pannell@gmail.com>
2020-07-20 00:51:39 -0700

    Initialize jest-07

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '\\.module\\.css$': 'identity-obj-proxy',
+    '\\.css$': require.resolve('./test/style-mock.js'),
+  },
+  snapshotSerializers: ['jest-emotion'],
+}
