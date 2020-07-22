083720c90d04198a205023a90aedf9cf4cab115d
wil pannell <wil.pannell@gmail.com>
2020-07-22 01:31:11 -0700

    11-support-a-test-utilities-file-with-jest-moduledirectories (emotion themes)

--- src/shared/calculator-display.spec.js
+++ src/shared/calculator-display.spec.js
@@ -1,23 +1,29 @@
 import React from 'react'
 import {render} from '@testing-library/react'
+import {ThemeProvider} from 'emotion-theming'
+import {dark} from 'themes'
 import CalculatorDisplay from './calculator-display'
 
 test('renders', () => {
-  const {container} = render(<CalculatorDisplay value="0" />)
+  const {container} = render(
+    <ThemeProvider theme={dark}>
+      <CalculatorDisplay value="0" />
+    </ThemeProvider>,
+  )
   expect(container.firstChild).toMatchInlineSnapshot(`
     .emotion-0 {
       position: relative;
-      color: white;
-      background: #1c191c;
       line-height: 130px;
       font-size: 6em;
       -webkit-flex: 1;
       -ms-flex: 1;
       flex: 1;
+      color: white;
+      background: #1c191c;
     }
 
     <div
-      class="emotion-0"
+      class="emotion-0 emotion-1"
     >
       <div
         class="autoScalingText"
