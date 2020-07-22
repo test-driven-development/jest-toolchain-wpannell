67dd09deeca67ea4cc2258147722c527366fb4bd
wil pannell <wil.pannell@gmail.com>
2020-07-21 18:03:35 -0700

    05-support-importing-css-files-with-jest-s-modulenamemapper

--- src/shared/auto-scaling-text.js
+++ src/shared/auto-scaling-text.js
@@ -2,6 +2,8 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styles from './auto-scaling-text.module.css'
 
+console.log(styles)
+
 function getScale(node) {
   if (!node) {
     return 1
